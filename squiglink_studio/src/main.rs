bf2c6e998e15d6952961902169fead3f43cba14c
Andrei Zhigalkin <snacks64@proton.me>
2024-11-23 19:17:40 +0500

    Remove the root route

--- squiglink_studio/src/main.rs
+++ squiglink_studio/src/main.rs
@@ -1,15 +1,9 @@
-use axum::routing::get;
-
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let router = axum::Router::new().route("/", get(root));
+    let router = axum::Router::new();
 
     let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(tcp_listener, router).await?;
 
     Ok(())
-}
-
-async fn root() -> &'static str {
-    "Hello, world!"
 }
