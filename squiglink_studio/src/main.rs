59f8da3a22746e58e4a202e1b26458afdf77ad03
Andrei Zhigalkin <snacks64@proton.me>
2024-11-23 19:13:07 +0500

    Rename the router variable

--- squiglink_studio/src/main.rs
+++ squiglink_studio/src/main.rs
@@ -2,10 +2,10 @@ use axum::routing::get;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let app = axum::Router::new().route("/", get(root));
+    let router = axum::Router::new().route("/", get(root));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    axum::serve(listener, app).await?;
+    axum::serve(listener, router).await?;
 
     Ok(())
 }
