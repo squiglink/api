c24ea97fde3b2435cf73b7967c119fc889959e74
Andrei Zhigalkin <snacks64@proton.me>
2024-12-23 00:02:24 +0500

    Expand the test for the database endpoint

--- src/routes/databases.test.ts
+++ src/routes/databases.test.ts
@@ -4,27 +4,55 @@ import { newDatabase } from "../database.js";
 
 describe("GET /databases", () => {
   test("it works", async () => {
-    const database = newDatabase();
-    const { id: userId } = await database
-      .insertInto("users")
-      .values({
-        display_name: "Foo",
-        scoring_system: "five_star",
-        username: "foo",
-      })
-      .returning("id")
-      .executeTakeFirstOrThrow();
-    const { id: databaseId } = await database
-      .insertInto("databases")
-      .values({ kind: "earbuds", path: "/", user_id: userId })
-      .returning("id")
-      .executeTakeFirstOrThrow();
+    let userIds = [];
+    let databaseIds = [];
 
-    const response = await application.request("/databases");
+    for (let index = 1; index <= 11; index++) {
+      const database = newDatabase();
+      const { id: userId } = await database
+        .insertInto("users")
+        .values({
+          display_name: `User ${index}`,
+          scoring_system: "five_star",
+          username: `foo_${index}`,
+        })
+        .returning("id")
+        .executeTakeFirstOrThrow();
+      userIds.push(userId);
+      const { id: databaseId } = await database
+        .insertInto("databases")
+        .values({ kind: "earbuds", path: "/", user_id: userId })
+        .returning("id")
+        .executeTakeFirstOrThrow();
+      databaseIds.push(databaseId);
+    }
 
-    expect(response.ok).toBe(true);
-    expect(await response.json()).toEqual([
-      { id: databaseId, kind: "earbuds", path: "/", user_id: userId },
-    ]);
+    let firstPageDatabases = [
+      { id: databaseIds[0], kind: "earbuds", path: "/", user_id: userIds[0] },
+      { id: databaseIds[1], kind: "earbuds", path: "/", user_id: userIds[1] },
+      { id: databaseIds[2], kind: "earbuds", path: "/", user_id: userIds[2] },
+      { id: databaseIds[3], kind: "earbuds", path: "/", user_id: userIds[3] },
+      { id: databaseIds[4], kind: "earbuds", path: "/", user_id: userIds[4] },
+      { id: databaseIds[5], kind: "earbuds", path: "/", user_id: userIds[5] },
+      { id: databaseIds[6], kind: "earbuds", path: "/", user_id: userIds[6] },
+      { id: databaseIds[7], kind: "earbuds", path: "/", user_id: userIds[7] },
+      { id: databaseIds[8], kind: "earbuds", path: "/", user_id: userIds[8] },
+      { id: databaseIds[9], kind: "earbuds", path: "/", user_id: userIds[9] },
+    ];
+    let secondPageDatabases = [
+      { id: databaseIds[10], kind: "earbuds", path: "/", user_id: userIds[10] },
+    ];
+
+    const pagelessResponse = await application.request("/databases");
+    expect(pagelessResponse.ok).toBe(true);
+    expect(await pagelessResponse.json()).toEqual(firstPageDatabases);
+
+    const firstPageResponse = await application.request("/databases?page=1");
+    expect(firstPageResponse.ok).toBe(true);
+    expect(await firstPageResponse.json()).toEqual(firstPageDatabases);
+
+    const secondPageResponse = await application.request("/databases?page=2");
+    expect(secondPageResponse.ok).toBe(true);
+    expect(await secondPageResponse.json()).toEqual(secondPageDatabases);
   });
 });
