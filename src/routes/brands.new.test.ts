281751dd910a6339145e735d35736afe9c4f452d
Aleksei Gostev <starawayy@protonmail.com>
2025-03-22 15:25:13 +0500

    Fix `/brands/new` test

--- src/routes/brands.new.test.ts
+++ src/routes/brands.new.test.ts
@@ -1,14 +1,34 @@
 import application from "../application.js";
 import { count } from "../test_helper.js";
+import { database } from "../database.js";
 import { describe, expect, test } from "vitest";
+import { signIn } from "../test_helper.js";
 
 describe("POST /brands/new", () => {
   test("creates a new brand", async () => {
+    const user = await database.transaction().execute(async (transaction) => {
+      return await transaction
+        .insertInto("users")
+        .values({
+          display_name: "Test User",
+          email: "test@test.com",
+          scoring_system: "five_star",
+          username: "test",
+        })
+        .returningAll()
+        .executeTakeFirstOrThrow();
+    });
+
+    const { accessToken } = await signIn(user.id);
+
     let body = { name: "Brand" };
 
     const response = await application.request("/brands/new", {
       body: JSON.stringify(body),
       method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
     });
 
     expect(await response.json()).toMatchObject(body);
