f4c2b36fccd1625a4b5350ccff8f94b317c26079
Aleksei Gostev <starawayy@protonmail.com>
2025-03-22 16:08:34 +0500

    Add tests for `authorization/verify` route

---
+++ src/routes/authorization.verify.test.ts
@@ -0,0 +1,80 @@
+import { database } from "../database.js";
+import { getRandomEmail } from "../test_helper.js";
+import { describe, expect, test, beforeEach } from "vitest";
+import { createJwtToken } from "../services/create_jwt_token.js";
+
+import application from "../application.js";
+import configuration from "../configuration.js";
+
+describe("GET /authorization/verify", () => {
+  test("returns 401 if token is not provided", async () => {
+    const response = await application.request("/authorization/verify", {
+      method: "GET",
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  test("returns 401 if token is not valid", async () => {
+    const response = await application.request("/authorization/verify?token=invalid-token", {
+      method: "GET",
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  test("returns 401 if user associated with token is not found", async () => {
+    const invalidToken = await createJwtToken(configuration.jwtExpirationTimeMagicLinkToken * 1000);
+    const response = await application.request(`/authorization/verify?token=${invalidToken}`, {
+      method: "GET",
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  test("returns 401 if token is expired", async () => {
+    const expiredToken = await createJwtToken(0);
+    const response = await application.request(`/authorization/verify?token=${expiredToken}`, {
+      method: "GET",
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  test("returns 200 with accessToken and refreshToken if token is valid", async () => {
+    const magicLinkToken = (
+      await database.transaction().execute(async (transaction) => {
+        const user = await transaction
+          .insertInto("users")
+          .values({
+            email: getRandomEmail(),
+            display_name: "Test User",
+            scoring_system: "five_star",
+            username: "test_user",
+          })
+          .returningAll()
+          .executeTakeFirstOrThrow();
+
+        const authToken = await createJwtToken(
+          configuration.jwtExpirationTimeMagicLinkToken * 1000,
+        );
+
+        return await transaction
+          .insertInto("jwt_magic_link_tokens")
+          .values({ token: authToken, user_id: user.id })
+          .returning("token")
+          .executeTakeFirstOrThrow();
+      })
+    ).token;
+
+    const response = await application.request(`/authorization/verify?token=${magicLinkToken}`, {
+      method: "GET",
+    });
+
+    const response_json = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(response_json).toHaveProperty("accessToken");
+    expect(response_json).toHaveProperty("refreshToken");
+  });
+});
