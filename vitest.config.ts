914cae72383fbf512e4806b064bcd44e7cd6f7cb
Aleksei Gostev <starawayy@protonmail.com>
2025-03-22 15:32:15 +0500

    Fix database configuration in test environment

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import configuration from "./src/configuration.js";
 
 export default defineConfig({
   test: {
@@ -6,6 +7,7 @@ export default defineConfig({
     setupFiles: ["src/test_helper.ts"],
     env: {
       SQUIGLINK_APPLICATION_ENVIRONMENT: "test",
+      SQUIGLINK_POSTGRES_DATABASE: configuration.postgresTestDatabase,
     },
   },
 });
