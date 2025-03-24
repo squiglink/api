8a1ddd763e10b43180f65d66ddc44b7bddc54223
Aleksei Gostev <starawayy@protonmail.com>
2025-03-24 19:27:39 +0500

    Use `vitest.config.ts` to setup environment variables for tests (#43)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vitest/config";
+import configuration from "./src/configuration.js";
 
 export default defineConfig({
   test: {
+    env: {
+      SQUIGLINK_POSTGRES_DATABASE: configuration.postgresTestDatabase,
+    },
     fileParallelism: false,
     setupFiles: ["src/test_helper.ts"],
   },
