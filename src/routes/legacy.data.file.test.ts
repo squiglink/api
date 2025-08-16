effc092da313e70e4746a62d4b2a1830d0807008
Andrei Zhigalkin <snacks64@proton.me>
2025-08-16 11:16:04 +0500

    Add the legacy data file endpoint

---
+++ src/routes/legacy.data.file.test.ts
@@ -0,0 +1,37 @@
+import { database } from "../database.js";
+import { describe, expect, it } from "vitest";
+import { insertMeasurement } from "../test_helper.factories.js";
+import application from "../application.js";
+
+describe("GET /legacy/data/:file_name", () => {
+  it("responds with success and returns the left channel measurement", async () => {
+    const { measurement } = await database.transaction().execute(async (transaction) => {
+      const measurement = await insertMeasurement(transaction);
+
+      return { measurement };
+    });
+
+    const response = await application.request(`/legacy/data/${measurement.id} L.txt`);
+    expect(await response.text()).toEqual(measurement.left_channel);
+    expect(response.ok).toBe(true);
+  });
+
+  it("responds with success and returns the right channel measurement", async () => {
+    const { measurement } = await database.transaction().execute(async (transaction) => {
+      const measurement = await insertMeasurement(transaction);
+
+      return { measurement };
+    });
+
+    const response = await application.request(`/legacy/data/${measurement.id} R.txt`);
+    expect(await response.text()).toEqual(measurement.right_channel);
+    expect(response.ok).toBe(true);
+  });
+
+  it("responds with not found if the measurement does not exist", async () => {
+    const response = await application.request(
+      `/legacy/data/67e1a0b0-d2a9-4f71-bf21-76b2ace7657e L.txt`,
+    );
+    expect(response.status).toBe(404);
+  });
+});
---
+++ src/routes/legacy.data.file.ts
@@ -0,0 +1,24 @@
+import { database } from "../database.js";
+import { Hono } from "hono";
+
+const application = new Hono();
+
+application.get("/legacy/data/:file_name{.* (L|R)\.txt}", async (context) => {
+  const [id, channelAndExtension] = context.req.param("file_name").split(" ");
+  const [channel, _extension] = channelAndExtension.split(".");
+
+  const result = await database
+    .selectFrom("measurements")
+    .select(["left_channel", "right_channel"])
+    .where("id", "=", id)
+    .executeTakeFirst();
+
+  if (result == undefined) {
+    return context.body(null, 404);
+  } else {
+    if (channel == "L") return context.text(result.left_channel);
+    if (channel == "R") return context.text(result.right_channel);
+  }
+});
+
+export default application;
