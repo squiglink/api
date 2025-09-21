0a4f396dac2c0d4ceec0119a7a28c6f7407dd754
Andrei Zhigalkin <41530422+snacks02@users.noreply.github.com>
2025-09-21 20:57:59 +0500

    Add Cloudflare Turnstile (#208)

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   test: {
     env: {
       SQUIGLINK_API_ENVIRONMENT: "test",
+      SQUIGLINK_CLOUDFLARE_TURNSTILE_ENABLED: "false",
+      SQUIGLINK_CLOUDFLARE_TURNSTILE_SECRET: "placeholder",
       SQUIGLINK_POSTGRES_DATABASE: configuration.postgresTestDatabase,
     },
     fileParallelism: false,
